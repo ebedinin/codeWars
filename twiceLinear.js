30a8155ab5a61aab430e0329ccd758e3f92eca49
ebedinin <bedinin@gmail.com>
2021-12-01 21:47:35 +1000

    Twice linear

--- twiceLinear.js
+++ twiceLinear.js
@@ -1,20 +1,17 @@
 const {log} = console;
-const findIndexYZ = (arr, y, z) => {
-
-};
-
 function dblLinear(n) {
 
     const arr = [1, 3, 4, 7, 9, 10];
     if (n < arr.length - 1) return arr[n];
     let i = 2;
-    while (arr.length < n - 1) {
+    const length = Math.floor(n*1.3);
+    while (arr.length < length) {
         const y = arr[i] * 2 + 1;
         const z = arr[i] * 3 + 1;
         let findY = true, findZ = true;
         let indexY = arr.length - 1;
         let indexZ = arr.length - 1;
-        for (let j = arr.length - 1; j >= 0; j--) {
+        for (let j = arr.length - 1; j >=  Math.floor(arr.length/4*3); j--) {
             findY = findY ? arr[j] !== y : false;
             findZ = findZ ? arr[j] !== z : false;
             if (!findY && !findZ) break;
@@ -28,7 +25,6 @@ function dblLinear(n) {
         [indexY, indexZ] = [indexY + 1, indexZ + 1];
         if (findY && findZ) {
             if (indexY === indexZ) {
-                let item;
                 if (y === z) {
                     arr.splice(indexY, 0, y);
                 }
@@ -50,17 +46,7 @@ function dblLinear(n) {
             if (findY) arr.splice(indexY, 0, y);
             if (findZ) arr.splice(indexZ, 0, z);
         }
-        /*const isContainsY = arr.indexOf(y)!== -1;
-        const isContainsZ = arr.indexOf(z)!== -1;
-
-        if (!isContainsY && !isContainsZ) continue;
-        if(isContainsY ^ isContainsZ){
-            const item = isContainsY?y:z;
-
-
-        }*/
         i++;
     }
-    return arr
+    return arr[n]
 }
-log(dblLinear(13)[10]);
